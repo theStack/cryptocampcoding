7de737ceceb8282a6b4178f0dec2ab8732fb504e
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-06-22 16:57:46 +0200

    fast_exp: add calculation of squarings table (a_0...a_255)

--- week1.zig
+++ week1.zig
@@ -1,10 +1,22 @@
 const std = @import("std");
 
-// week 1, exercise 1
+// week 1, exercise 1: use square-and-multiply algorithm
 fn fast_exp(base: u256, exponent: u256, modulus: u256) u256 {
+    var squarings_table: [256]u256 = undefined;
+    var a_i: u256 = undefined;
+    for (0..256) |i| {
+        if (i == 0) {
+            a_i = base;
+        } else {
+            a_i = (a_i * a_i) % modulus;
+        }
+        squarings_table[i] = a_i;
+    }
+
+    const result: u256 = 42;
     std.debug.print("Calculating {d}, {d}, {d}...\n", .{base, exponent, modulus});
     // TODO: implement
-    return 42;
+    return result;
 }
 
 pub fn main() !void {
