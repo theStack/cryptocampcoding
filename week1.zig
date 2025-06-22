5209a5b3ac8346fedc5800efd032875712f984df
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-06-22 16:28:34 +0200

    use larger test values for fast_exp (that would overflow w/o modulus)

--- week1.zig
+++ week1.zig
@@ -9,6 +9,10 @@ fn fast_exp(base: u256, exponent: u256, modulus: u256) u256 {
 
 pub fn main() !void {
     std.debug.print("Hello cryptocamp!\n", .{});
-    const result = fast_exp(23, 42, 1337);
+    const result = fast_exp(123, 42, 31337);
+    // expected according to Python:
+    //
+    // $ python3 -c "print((123**42)%31337)"
+    // 12516
     std.debug.print("Answer: {d}\n", .{result});
 }
